d65c306f3645ec6f11a47e98583f72b0481974e7
Yuvraj Singh Chouhan <root@localhost.localdomain>
2020-09-03 17:22:34 +0530

    initial commit 1

---
+++ Day4/hello/src/test/java/org/tektutor/AppTest.java
@@ -0,0 +1,19 @@
+package org.tektutor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class  AppTest {
+
+	@Test
+	public void testSayHello() {
+		App app = new App();
+		
+		String actualResponse = app.sayHello();
+		String expectedResponse = "Hello Java!";
+
+		assertEquals ( expectedResponse, actualResponse );
+
+	}
+
+}
